5aa09402d5807f7b10c471ef073a689bfebb4db4
SebastianMCarreira <sebaxcarr@gmail.com>
2018-09-09 13:10:10 -0300

    Automatic loading of jQuery, selection of users and sending of message

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import pyautogui
+import random
 import time
 import json
 import os
 import re
+import pyautogui
+import pyperclip
 from bs4 import BeautifulSoup
 from random import randint
 from pprint import pprint
@@ -20,6 +23,9 @@ pyautogui.typewrite("Hello World!")
 time.sleep(randint(1,5))
 pyautogui.press('enter')'''
 
+def randSleep(min = 0.5, max = 1):
+    time.sleep(random.uniform(min, max))
+
 def firstChild(item):
     return list(item)[0]
 
@@ -33,12 +39,12 @@ def getContactInfo(contact, selected_contact):
     else:
         unread = int(unread)
 
-    LstMsg=LstMsg.strip("\n").strip(" ").strip("\n")
+    LstMsg=LstMsg
     contact = contact[0]
     Name = firstChild(firstChild(firstChild(contact))).string
-    Name = Name.strip("\n").strip(" ").strip("\n")
+    Name = Name
     TimeLstMsg = firstChild(list(contact)[1]).string
-    TimeLstMsg=TimeLstMsg.strip("\n").strip(" ").strip("\n")
+    TimeLstMsg=TimeLstMsg
     if selected_contact == Name:
         selected = True
     else:
@@ -80,41 +86,95 @@ def getChatInfo(chat):
     messages = [getMessageInfo(x, selected_contact_name) for x in messages if isMessage(x, selected_contact_name)]
     return {'selected_contact': selected_contact_name, 'messages': messages}
 
+def loadJQuery():
+    pyautogui.hotkey('ctrl','shift','j')
+    time.sleep(3)
+    with open('jquery.js','r',encoding='utf-8') as f:
+        jquery = f.read()
+        pyperclip.copy(jquery)
+        pyautogui.hotkey('ctrl','v')
+        time.sleep(3)
+        pyautogui.press('enter')
+    pyautogui.hotkey('ctrl','shift','j')
+
+def sendMessage(Txt, To):
+    #Focus on search input
+    pyautogui.hotkey('ctrl','shift','j')
+    randSleep()
+    pyautogui.typewrite("$('input')[0].focus()")
+    pyautogui.press('enter')
+    pyautogui.hotkey('ctrl','shift','j')
+    #Type contact name on search input
+    randSleep()
+    pyautogui.typewrite(To)
+    randSleep()
+    #Tab to select contact and focus on message input
+    pyautogui.press('tab')
+    pyautogui.press('tab')
+    pyautogui.press('tab')
+    time.sleep(1)
+    #Send message
+    pyautogui.typewrite(Txt)
+    randSleep()
+    pyautogui.press('enter')
+    #Focus on search input
+    pyautogui.hotkey('ctrl','shift','j')
+    randSleep()
+    pyautogui.typewrite("$('input')[0].focus()")
+    pyautogui.press('enter')
+    pyautogui.hotkey('ctrl','shift','j')
+    #Clear search input
+    randSleep()
+    pyautogui.press('esc')
+
+
+sent = False
+
+time.sleep(5)
+loadJQuery()
 os.chdir('htmls')
 while True:
     READED = os.listdir('READED')
     for f in os.listdir():
         if f.endswith('.json') and f not in READED:
-            with open(f, 'r', encoding='utf-8') as file:
-                obj = json.loads(file.read())
-                print(obj['URL'])
-                soup = BeautifulSoup(obj['HTML'], 'html.parser')
-                for svg in soup('svg'):
-                    svg.decompose()
-                for head in soup('head'):
-                    head.decompose()
-                for script in soup('script'):
-                    script.decompose()
-                
+            try:
+                with open(f, 'r', encoding='utf-8') as file:
+                    obj = json.loads(file.read())
+                    print(obj['URL'])
+                    soup = BeautifulSoup(obj['HTML'], 'html.parser')
+                    for svg in soup('svg'):
+                        svg.decompose()
+                    for head in soup('head'):
+                        head.decompose()
+                    for script in soup('script'):
+                        script.decompose()
+                    
 
-                #newfilename = f[:-4] + "html"
-                #with open(newfilename, 'w', encoding='utf-8') as nf:
-                #    nf.write(soup.prettify())
+                    #newfilename = f[:-4] + "html"
+                    #with open(newfilename, 'w', encoding='utf-8') as nf:
+                    #    nf.write(soup.prettify())
 
-                fidiv=list(soup.find('div',{'id':'app'}).children)[0]#_1FKgS
-                sediv=list(list(fidiv.children)[5].children)#_3dqpi
-                contactos=list(list(sediv[2])[0])[3]#_1vDUw
-                contactos=list(firstChild(firstChild(firstChild(contactos))))#RLfQR
-                contactos=[x for x in contactos if x != '\n']
-                
-                chat=list(firstChild(sediv[3]))#_1GX8_
-                chat = getChatInfo(chat)
-                pprint(chat)
-                for contact in contactos:
-                    pprint(getContactInfo(contact,chat['selected_contact']))
-            
+                    fidiv=list(soup.find('div',{'id':'app'}).children)[0]#_1FKgS
+                    sediv=list(list(fidiv.children)[5].children)#_3dqpi
+                    contactos=list(list(sediv[2])[0])[3]#_1vDUw
+                    contactos=list(firstChild(firstChild(firstChild(contactos))))#RLfQR
+                    contactos=[x for x in contactos if x != '\n']
+                    
+                    chat=list(firstChild(sediv[3]))#_1GX8_
+                    chat = getChatInfo(chat)
+                    pprint(chat)
+                    for contact in contactos:
+                        pprint(getContactInfo(contact,chat['selected_contact']))
+            except:
+                #While having something on the contact search input, the HTML changes heavily and the getContactInfo fails
+                pass
             os.rename('.\\'+f,'.\\READED\\'+f)
     time.sleep(1)
     newjsons = [x for x in os.listdir() if x.endswith('json')]
+
+    if not sent:
+        sendMessage("Hola","Sebastian")
+        sent = True
+
     if len(newjsons) > 0:
         os.system('cls')
